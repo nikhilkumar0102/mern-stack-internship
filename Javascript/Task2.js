0c86cc28976b1689c7d7f6743e77310086bf8dae
Nikhil Kumar <nikhilkumar01.name@gmail.com>
2025-03-25 02:06:19 +0530

    Update and rename Task2.js to Task2.html

--- Javascript/Task2.js
+++
@@ -1,6 +0,0 @@
-function sortStringAlphabetically(str) {
-    return str.split("").sort().join(""); 
-}
-
-let exampleString = "webmaster";
-console.log(sortStringAlphabetically(exampleString)); // Expected Output: 'abeemrstw'
